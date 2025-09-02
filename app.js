4685218cd2f6b11ed964be9d1849039f714eb953
Emanoele22 <emanoele.conceicao@ba.estudante.senai.br>
2025-09-01 23:12:28 -0300

    Com rotaaaaaaaasgit branch -M maingit branch -M main!

---
+++ app.js
@@ -0,0 +1,103 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3020;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static('public'));
+
+// Rotas corrigidas
+app.get("/", (req, res) => {
+  res.render("INICIO/inicial");
+});
+
+app.get("/login", (req, res) => {
+  res.render("LOGIN/login");
+});
+
+app.get("/perfil", (req, res) => {
+  res.render("PERFIL/perfil");
+});
+
+app.get("/home", (req, res) => {
+  res.render("HOME/home");
+});
+
+// URLs corrigidas - sem espaços e acentos
+app.get("/trocar-senha", (req, res) => {
+  res.render("SENHA/senha");
+});
+
+app.get("/codigo-confirmacao", (req, res) => {
+  res.render("SENHA/codigo");
+});
+
+app.get("/nova-senha", (req, res) => {
+  res.render("SENHA/trocar");
+});
+
+app.get("/senha-trocada", (req, res) => {
+  res.render("SENHA/pronto");
+});
+
+app.get("/opcoes-aluno", (req, res) => {
+  res.render("ALUNO/inicio");
+});
+
+app.get("/cadastro-aluno", (req, res) => {
+  res.render("ALUNO/cadastro");
+});
+
+app.get("/cadastro-responsavel", (req, res) => {
+  res.render("ALUNO/cadastro1");
+});
+
+app.get("/criancas-cadastradas", (req, res) => {
+  res.render("ALUNO/cria");
+});
+
+app.get("/opcoes-financeiro", (req, res) => {
+  res.render("FINANCEIRO/bot");
+});
+
+app.get("/relatorio", (req, res) => {
+  res.render("FINANCEIRO/relatorio");
+});
+
+app.get("/relatorio-diario", (req, res) => {
+  res.render("FINANCEIRO/rel_diario");
+});
+
+app.get("/relatorio-mensal", (req, res) => {
+  res.render("FINANCEIRO/rel_mensal");
+});
+
+app.get("/fluxo-de-caixa", (req, res) => {
+  res.render("FINANCEIRO/fluxo_de_caixa");
+});
+
+app.get("/relatorio-financeiro", (req, res) => {
+  res.render("FINANCEIRO/financial_reprt_page");
+});
+
+app.get("/adimplentes-inadimplentes", (req, res) => {
+  res.render("FINANCEIRO/adimplentes");
+});
+
+app.get("/opcoes-turma", (req, res) => {
+  res.render("TURMA/index");
+});
+
+app.get("/cadastro-turma", (req, res) => {
+  res.render("TURMA/cadastro");
+});
+
+app.get("/turmas-cadastradas", (req, res) => {
+  res.render("TURMA/acessar");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ public/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cimatec',
+    database: 'cadastrar2'
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/cadastro.html')
+});
+
+app.post('/cadastrar', (req, res) => {
+    const {professor, turma, turno, quantidade_alunos, faixa_etaria} = req.body;
+
+    const sql = "INSERT INTO turma (professor, turma, turno, quantidade_alunos, faixa_etaria) VALUES (?, ?, ?, ?, ?)";
+    db.query(sql, [professor, turma, turno, quantidade_alunos, faixa_etaria], (err, result) => {
+        if (err) throw err;
+        res.send("Turma cadastrada com sucesso!");
+    });
+});
+
+app.listen(3010, () => {
+    console.log("Servidor rodando em http://localhost:3010")
+});
